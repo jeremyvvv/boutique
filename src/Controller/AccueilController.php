d17c7ca6597eea7ec6213565815809953fb5bb36
j.vassant <jeremy.vassant@gmail.com>
2020-12-02 12:54:47 +0100

    Commit 02/12/2020

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Produit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +13,12 @@ class AccueilController extends AbstractController
     /**
      * @Route("/", name="accueil")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $manager): Response
     {
+        $produits = $manager->getRepository(Produit::class)->findAll();
         return $this->render('accueil/index.html.twig', [
             'controller_name' => 'AccueilController',
+            'produits' => $produits
         ]);
     }
 }
--- src/Entity/Produit.php
+++ src/Entity/Produit.php
@@ -33,6 +33,48 @@ class Produit
      */
     private $tarif;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @return mixed
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param mixed $description
+     */
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param mixed $image
+     */
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+    }
+
     public function __toString()
     {
         return $this->libelle;
@@ -78,4 +120,6 @@ class Produit
 
         return $this;
     }
+
+
 }
